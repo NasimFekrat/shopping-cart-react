9e9a806d223bd3e2f43dc2955f0a5b04e0b43785
Nasim Fekrat <nasimfekrat@yahoo.com>
2024-08-15 10:10:00 -0400

    using HashRouter for deployment in GitHub pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Shop from "./pages/shop/shop";
