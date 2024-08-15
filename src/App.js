f8c723ac1a98af38d1589ccc6875c9828f2ae6e4
Nasim Fekrat <nasimfekrat@yahoo.com>
2024-08-15 14:32:57 -0400

    Using Hash Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Shop from "./pages/shop/shop";
@@ -14,8 +14,8 @@ function App() {
         <Router>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Shop />} />
-            <Route path="/cart" element={<Cart />} />
+            <Route path="/#" element={<Shop />} />
+            <Route path="/#/cart" element={<Cart />} />
           </Routes>
           <Footer />
         </Router>
