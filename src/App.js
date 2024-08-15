a699445cbc7f51486efd465ff51ec8bd0325e863
Nasim Fekrat <nasimfekrat@yahoo.com>
2024-08-15 10:12:26 -0400

    remove basename

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <ShopContextProvider>
-        <Router basename={process.env.PUBLIC_URL}>
+        <Router>
           <Navbar />
           <Routes>
             <Route path="/" element={<Shop />} />
